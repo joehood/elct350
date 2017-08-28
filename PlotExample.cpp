c189a87d3753cd7471854762cff971a3583e8e8a
Joe Hood <josephmhood@gmail.com>
2017-08-28 17:09:01 -0400

    Update PlotExample.cpp

--- PlotExample.cpp
+++ PlotExample.cpp
@@ -3,24 +3,27 @@
 
 int main()
 {
-    ComplexMatrix A(2, 2);
-    ComplexColumnVector B(2);
-    ComplexColumnVector X(2);
+    Matrix A(2, 2);
+    ColumnVector B(2);
+    ColumnVector X(2);
     
     Plotter plotter("State Space Test");
     
-    A(1, 2) = Complex(1.0, 0.5);
-    A(2, 1) = Complex(-2.0, 1.0);
-    A(2, 2) = Complex(-1.0, 0.5);
-    B(2) = Complex(5.0);
+    A(1, 2) = 1.0;
+    A(2, 1) = -2.0;
+    A(2, 2) = -1.0;
+    B(2) = 5.0;
     
-    double dt = 0.05;
+    double h = 0.05;
+    double t = 0.0;
+    double tmax = 5.0;
     double u = 10.0;
     
-    for (double t = 0.0; t < 5.0; t += 0.05)
+    while (t <= tmax)
     {
-        X += (A * X + B * u) * dt;
-        plotter.AddRow(t, X(1).real(), X(2).real());
+        X += (A * X + B * u) * h;
+        plotter.AddRow(t, X(1), X(2));
+        t += h;
     }
     
     plotter.SetLabels("x1", "x2");
